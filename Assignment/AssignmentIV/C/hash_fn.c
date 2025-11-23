044109bd390251d12009563c191552a577e49e49
謝昕芳 <ayvonne101122@gmail.com>
2025-11-23 17:12:33 +0000

    Update C++

--- Assignment/AssignmentIV/C/hash_fn.c
+++ Assignment/AssignmentIV/C/hash_fn.c
@@ -16,12 +16,21 @@
 #include "hash_fn.h"
 
 int myHashInt(int key, int m) {
-    // TODO: replace with your own design
+    if(key<0)
+    {
+    key=-key;
+    }
+    // 把負數改成正的
     return key % m;  // division method example
 }
 
 int myHashString(const char* str, int m) {
     unsigned long hash = 0;
+     while(*str){
+        hash = hash * 33 + (unsigned char)(*str); // 33=2^5+1電腦比較好計算，上課例題也推薦
+        //  把str限定成正整數
+        str++; // 往後挪一個字元
+    }
     // TODO: replace with your own design
     return (int)(hash % m); // basic division method
 }
--- Assignment/AssignmentIV/CXX/hash_fn.cpp
+++ Assignment/AssignmentIV/CXX/hash_fn.cpp
@@ -13,14 +13,24 @@
    Developer: Yu-Feng Huang <yfhuang@saturn.yzu.edu.tw>
  */
 #include "hash_fn.hpp"
+#include<bits/stdc++.h>
 
 int myHashInt(int key, int m) {
-    // TODO: replace with your own design
+    if (key<0)
+    {
+        key=-key;//如果hash是負的轉成正的
+    } // TODO: replace with your own design
     return key % m;  // basic division method
 }
 
+
 int myHashString(const std::string& str, int m) {
     unsigned long hash = 0;
-    // TODO: replace with your own design
+    const char* a = str.c_str();//str.data()因為版本問題不能執行改string，爬文這個函數在這個版本才有效
+    while(*a){
+        hash = hash * 33 + (unsigned char)(*a); // 33=2^5+1電腦比較好計算，上課例題也推薦
+        //  把str限定成正整數
+        a++; // 往後挪一個字元
+    } // TODO: replace with your own design
     return static_cast<int>(hash % m);  // basic division method
 }
